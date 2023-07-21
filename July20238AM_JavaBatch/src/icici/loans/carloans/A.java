61e5ce37d5b18c040040db657cf8ec766856bed0
ravilella1234 <masters.selenium@gmail.com>
2023-07-21 09:08:37 +0530

    Added Java Docs

--- July20238AM_JavaBatch/src/icici/loans/carloans/A.java
+++ July20238AM_JavaBatch/src/icici/loans/carloans/A.java
@@ -1,26 +1,41 @@
 package icici.loans.carloans;
 
-import icici.loans.eduloans.C;
+import java.util.Date;
 
-public class A 
+public   class A 
 {	
+	public static final int x = 100;
+	
 	public void m1()
 	{
 		System.out.println("iam m1 from A");
+		
+		System.out.println(x);
 	}
+	
+	public static void m4()
+	{
+		System.out.println("iam m4 static method");
+	}
+	
+	static
+	{
+		System.out.println("iam static Block...");
+	}
+	
+	static
+	{
+		Date dt = new Date();
+		System.out.println(dt);
+	}
+	
+	public abstract void m6();
 
 	public static void main(String[] args) 
 	{
 		A a = new A();
-		a.m1();
-		
-		A obj;
-		
-		 obj = new B();
-		obj.m1();
-		
-		 obj = new C();
-		obj.m1();
+		a.m1();	
+		A.m4();
 	}
 
 }
