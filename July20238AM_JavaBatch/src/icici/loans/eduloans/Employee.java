f9cf7d78e03801e0ae3cddb247aae8cd8c349b61
ravilella1234 <masters.selenium@gmail.com>
2023-08-01 09:07:32 +0530

    Constructors

--- July20238AM_JavaBatch/src/icici/loans/eduloans/Employee.java
+++ July20238AM_JavaBatch/src/icici/loans/eduloans/Employee.java
@@ -4,19 +4,68 @@ public class Employee
 {
 	int eno;
 	String ename;
+	float sal;
 	
+	public Employee()
+    {
+		System.out.println("Iam default constructor...");
+		
+		System.out.println(this.eno);
+		System.out.println(this.ename);
+		System.out.println(this.sal);
+    }
+	
+	public Employee(int eno) 
+	{
+		this();
+		this.eno = eno;
+		
+		System.out.println(this.eno);
+		System.out.println(this.ename);
+		System.out.println(this.sal);
+	}
+	
+	public Employee(int eno, String ename) 
+	{
+		this(eno);
+		this.eno = eno;
+		this.ename = ename;
+		
+		System.out.println(this.eno);
+		System.out.println(this.ename);
+		System.out.println(this.sal);
+	}
+	
+	public Employee(int eno, String ename, float sal) 
+	{
+		this(eno,ename);
+		this.eno = eno;
+		this.ename = ename;
+		this.sal = sal;
+		
+		System.out.println(this.eno);
+		System.out.println(this.ename);
+		System.out.println(this.sal);
+	}
+
 	public static void main(String[] args)
 	{
-		Employee e1 = new Employee();
-		e1.eno=1744;
-		e1.ename="ravi";
+		/*Employee e1 = new Employee();
 		System.out.println(e1.eno);
 		System.out.println(e1.ename);
+		System.out.println(e1.sal);
 		
-		Employee e2 = new Employee();
-		e2.eno=1745;
-		e2.ename="sai";
+		Employee e2 = new Employee(1743);
 		System.out.println(e2.eno);
 		System.out.println(e2.ename);
+		System.out.println(e2.sal);
+		
+		Employee e3 = new Employee(1744,"ravi");
+		System.out.println(e3.eno);
+		System.out.println(e3.ename);
+		System.out.println(e3.sal);*/
+		
+		Employee e4 = new Employee(1745, "sai", 12.34f);
+		
 	}
 }
